e2ef845483658e4851168bb989a86b13d651539c
Nikos Patikas <nik.patik@gmail.com>
2020-10-27 12:14:20 +0000

    GZIP output extract-alignments.sh

--- utils/extract-alignments.sh
+++ utils/extract-alignments.sh
@@ -3,6 +3,6 @@
 BAMFILE=$1
 OUTFILE=$2
 
-OUTPUT="$OUTFILE/$(dirname $BAMFILE)"
+OUTPUT="$OUTFILE/$(dirname $BAMFILE).gz"
 echo $BAMFILE
-samtools view $BAMFILE |  awk -F'\t' '{print $1 "," $18}' | awk -F'_' '{print $3}' > $OUTPUT
+samtools view $BAMFILE |  awk -F'\t' '{print $1 "," $18}' | awk -F'_' '{print $3}' | gzip -c > $OUTPUT
